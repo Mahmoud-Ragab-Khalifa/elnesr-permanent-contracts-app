713c12e5fdf1eea2ca41878b01915ea6d97411f8
Mahmoud-Ragab-Khalifa <mahmoudragabkhalifaa@gmail.com>
2026-06-23 12:45:03 +0300

    feat: add login form ui

--- src/app/login/_components/LoginForm.tsx
+++ src/app/login/_components/LoginForm.tsx
@@ -1,7 +1,43 @@
 "use client";
 
 const LoginForm = () => {
-  return <form>LoginForm</form>;
+  return (
+    <form className="grid gap-6 rounded-lg glass-strong p-5 w-full max-w-md">
+      <h1 className="text-center font-bold italic text-primary glow-text text-xl">
+        سجل الدخول لأثبات هويتك
+      </h1>
+
+      <div className="grid gap-4">
+        <label htmlFor="email" className="text-primary font-bold">
+          أدخل بريدك الإلكتروني
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="input"
+          placeholder="ad@example.org"
+        />
+      </div>
+
+      <div className="grid gap-4">
+        <label htmlFor="password" className="text-primary font-bold">
+          أدخل كلمة المرور
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="input"
+          placeholder="•••••••"
+        />
+      </div>
+
+      <button type="submit" className="submit-btn">
+        تسجيل الدخول
+      </button>
+    </form>
+  );
 };
 
 export default LoginForm;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./_components/LoginForm";
 
 const LoginPage = () => {
   return (
-    <main>
+    <main className="min-h-dvh flex items-center justify-center p-5 md:p-0 glass">
       <LoginForm />
     </main>
   );
